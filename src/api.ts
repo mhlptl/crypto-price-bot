689e08c718c7cc7152fa3cf5a6b10ca4518dad0b
Mehul Patel <mehulp1123@gmail.com>
2021-04-24 22:40:57 -0400

    linked api call to discord portion of code

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,8 @@
-import axios, {AxiosResponse} from "axios";
+import axios from "axios";
+
+interface Params {
+	symbol: string;
+}
 
 const url: string = process.env.API_URL;
 
@@ -6,17 +10,31 @@ const header = {
 	"X-CMC_PRO_API_KEY": process.env.API_KEY
 };
 
-const params = {
-	symbol: "eth"
+const apiRequest = async (ticker: string, params: Params) => {
+	const config = {
+		headers: header,
+		params: params
+	};
+	try {
+		const res = await axios.get(url, config);
+		const data = await res.data;
+
+		if (data.status.error_code !== 0) {
+			return -1;
+		}
+		return data.data[ticker].quote.USD.price;
+	} catch (e) {
+		console.error(e);
+		return -1;
+	}
 };
 
-const config = {
-	headers: header,
-	params: params
-};
+const getPrice = async (ticker: string): Promise<number> => {
+	const params = {
+		symbol: ticker
+	};
 
-const getPrice = async (): Promise<AxiosResponse> => {
-	return await axios.get(url, config);
+	return await apiRequest(ticker, params);
 };
 
 export {getPrice};
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,28 @@
-// import Discord from 'discord.js';
-
+import Discord from "discord.js";
 import {getPrice} from "./api";
 
-const data = getPrice();
+const client = new Discord.Client();
 
-data.then((data) => {
-	const d: APIRequest = data.data;
-	console.log(d);
-	console.log(d.data["ETH"].quote.USD.price);
+const prefix = "$";
+
+client.on("message", async (msg: Discord.Message) => {
+	if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+
+	const commandBody = msg.content.slice(prefix.length);
+	const command = commandBody.split(" ").shift()?.toUpperCase();
+
+	if (command === undefined || command.length === 0) {
+		msg.reply("Please use a valid tag");
+		return;
+	}
+	// call api to get price
+	const price = await getPrice(command);
+
+	if (price === -1) {
+		msg.reply(`Cannot find cryptocurrency with ticker ${command}`);
+		return;
+	}
+	msg.reply(`Cryptocurrency with ticker ${command} has a price of $${price}`);
 });
 
-// const client = new Discord.Client();
-
-// const prefix = '$';
-
-// client.on('ready', () => {
-// 	console.log(`Logged in as ${client.user?.username}`);
-// });
-
-// client.on('message', (msg: Discord.Message) => {
-// 	if(msg.author.bot || !msg.content.startsWith(prefix)) return;
-
-// 	const commandBody = msg.content.slice(prefix.length);
-// 	const command = commandBody.split(' ').shift()?.toLowerCase();
-
-// 	if(command === undefined || command.length === 0) {
-// 		msg.reply('Please use a valid tag');
-// 	}
-// 	// call api to get price
-
-// 	msg.reply('Price: ');
-// });
-
-// client.login('token');
+client.login("token");
